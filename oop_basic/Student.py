0f002b87dd17649cb2b2cf46aaa1dfd7b0b37cf7
huchangchun <hu_changchun@126.com>
2018-01-01 23:20:47 +0800

    add learning samples

---
+++ oop_basic/Student.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+class Student():
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+    def get_name(self):
+        return self.__name
+    def get_score(self):
+        return self.__score
+    def print_score(self):
+        print('%s :%s'%(self.__name,self.__score))
+    #通过方法来修改属性，方法可以对参数做检查，避免传入无效的参数：
+    def set_score(self,score):
+        if 0<score<=100:
+            self.__score = score
+        else:
+            raise ValueError('Bad score')
+    def get_grade(self):
+        if self.__score >= 90:
+            return 'A'
+        elif self.__score >=60:
+            return 'B'
+        else:
+            return 'C'
+Joe = Student('Joe',59)
+
+print('Joe.name',Joe.get_name())
+Joe.set_score(90)
+print('Joe.score',Joe.get_score())
+print('Joe grade：%s'%(Joe.get_grade()))
+print('Do not user Joe.__Student__name:',Joe._Student__name)
+# Joe.name Joe
+# Joe.score 90
+# Joe grade：A
+# Do not user Joe.__Student__name: Joe
---
+++ oop_basic/get_type.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# type()
+print('type(123)=',type(123))
+print('type(\'123\')=',type('123'))
+print('type(None)=',type(None))
+print('type(abs)=',type(abs))
+import types
+print('type(\'abc\')==str?', type('abc')==str)
+# type(123)= <oop_basic 'int'>
+# type('123')= <oop_basic 'str'>
+# type(None)= <oop_basic 'NoneType'>
+# type(abs)= <oop_basic 'builtin_function_or_method'>
+# type('abc')==str? True
