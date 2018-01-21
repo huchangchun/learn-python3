ce1f639a8d1fc0f48517d3568e90cc5b554a7f87
huchangchun <hu_changchun@126.com>
2018-01-21 22:57:16 +0800

    add samples

--- practice/exam.py
+++ practice/exam.py
@@ -70,22 +70,78 @@ def writefile():
 import time
 t1 = "2017-12-31 23:40:00"
 t1_tuple = time.strptime(t1,"%Y-%m-%d %H:%M:%S")
-print(t1_tuple)
+# print(t1_tuple)
 t1_stamp = time.mktime(t1_tuple)
-print(t1_stamp)
+# print(t1_stamp)
 t1_fmt = time.strftime("%Y/%m/%d %H-%M-%S",t1_tuple)
-print(t1_fmt)
+# print(t1_fmt)
 
 #返回当前时间的时间戳
 now = time.time()
 twodayago = now - 60*60*24*2
 twodayago_tuple = time.localtime(twodayago)
 twodayago_fmt = time.strftime("%Y/%m/%d %H-%M-%S",twodayago_tuple)
-print("twodayago:%s" %twodayago_fmt)
+# print("twodayago:%s" %twodayago_fmt)
 nowfmt = time.localtime(now)
-print("今天是%s年%s月%s日 %s点%s分%s秒"%(time.strftime('%Y',nowfmt),
-                                time.strftime('%m',nowfmt),
-                                time.strftime('%d',nowfmt),
-                                time.strftime('%H',nowfmt),
-                                time.strftime('%M',nowfmt),
-                                time.strftime('%S',nowfmt)))
+ # print("今天是%s年%s月%s日 %s点%s分%s秒"%(time.strftime('%Y',nowfmt),
+ #                                time.strftime('%m',nowfmt),
+ #                                time.strftime('%d',nowfmt),
+ #                                time.strftime('%H',nowfmt),
+ #                                time.strftime('%M',nowfmt),
+ #                                time.strftime('%S',nowfmt)))
+
+"""
+将
+[5,5,1,1,2,3,2,2,32,2,2,3,2,2,32,2,3,3,3,3,2]
+转变为
+[[5, 5], [1, 1], [2], [3], [2, 2], [32], [2, 2], [3], [2, 2], [32], [2], [3, 3, 3, 3], [3]]
+"""
+lst =[5,5,1,1,2,3,2,2,32,2,2,3,2,2,32,2,3,3,3,3,2]
+
+def group_list(list):
+    new_lst = [[]]
+    count = len(list)
+    for i in range(count-1):
+        if list[i]==list[i+1]:
+            new_lst[-1].append(list[i])
+        else:
+            new_lst[-1].append(list[i])
+            new_lst.append([])
+    new_lst[-1].append(list[i])
+    return new_lst
+nlst = group_list(lst)
+# print(nlst)
+
+"""
+将
+[5,5,1,1,2,3,2,2,32,2,2,3,2,2,32,2,3,3,3,3,2]
+转变为
+[[5, 5], [1, 1], 2, 3, [2, 2], 32, [2, 2], 3, [2, 2], 32, 2, [3, 3, 3, 3], 2]
+append和extend都仅只可以接收一个参数
+append 任意，甚至是tuple
+extend 只能是一个列表
+"""
+def group_listtwo(list):
+    new_lst = []
+    templist=[]
+    count = len(list)
+    for i in range(count-1):
+        if list[i]==list[i+1]:
+            templist.append(list[i])
+        else:
+            templist.append(list[i])
+            if len(templist) >1:
+                new_lst.append([])
+                for j in templist:
+                    new_lst[-1].append(j)
+            else:
+                new_lst.extend(templist)
+            templist.clear()
+    if list[i+1] == new_lst[-1]:
+        new_lst[-1].append(list[i+1])
+    else:
+        new_lst.append(list[i+1])
+    return new_lst
+nlst = group_listtwo(lst)
+# print(nlst)
+
---
+++ time/do_time2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#time操作
+# 1)将字符串的时间 "2017-12-31 23:40:00" 转换为时间戳和时间元组
+# 2)字符串格式更改，如time = "2017-12-31 23:40:00" 改为time = "2017/12/31 23-40-00"
+# 3)获取两天前的当前时间并以可读的字符串形式（格式如1题）
+# 4)通过time输出时间'今天是2018年1月1日 08点08分08秒'
+import time
+t1 = "2017-12-31 23:40:00"
+t1_tuple = time.strptime(t1,"%Y-%m-%d %H:%M:%S")
+print(t1_tuple)
+t1_stamp = time.mktime(t1_tuple)
+print(t1_stamp)
+t1_fmt = time.strftime("%Y/%m/%d %H-%M-%S",t1_tuple)
+print(t1_fmt)
+
+#返回当前时间的时间戳
+now = time.time()
+twodayago = now - 60*60*24*2
+twodayago_tuple = time.localtime(twodayago)
+twodayago_fmt = time.strftime("%Y/%m/%d %H-%M-%S",twodayago_tuple)
+print("twodayago:%s" %twodayago_fmt)
+nowfmt = time.localtime(now)
+print("今天是%s年%s月%s日 %s点%s分%s秒"%(time.strftime('%Y',nowfmt),
+                                time.strftime('%m',nowfmt),
+                                time.strftime('%d',nowfmt),
+                                time.strftime('%H',nowfmt),
+                                time.strftime('%M',nowfmt),
+                                time.strftime('%S',nowfmt)))
+# time.struct_time(tm_year=2017, tm_mon=12, tm_mday=31, tm_hour=23, tm_min=40, tm_sec=0, tm_wday=6, tm_yday=365, tm_isdst=-1)
+# 1514734800.0
+# 2017/12/31 23-40-00
+# twodayago:2018/01/19 22-55-02
+# 今天是2018年01月21日 22点55分02秒
